bd5cfa8626e485b092e54d1f7802c9dc779b6f7f
morebo1 <sanketmore.39410@gamil.com>
2026-09-06 11:28:00 +0530

    fixing cors issuse

--- delivery-app-api/src/main/java/in/sanketmore/delivery/app/api/config/SecurityConfig.java
+++ delivery-app-api/src/main/java/in/sanketmore/delivery/app/api/config/SecurityConfig.java
@@ -65,10 +65,11 @@ public class SecurityConfig {
 
     private UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174","https://foodies-one-bay.vercel.app/","https://foodies-adminpanel-xi.vercel.app/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
         config.setAllowCredentials(true);
+
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
