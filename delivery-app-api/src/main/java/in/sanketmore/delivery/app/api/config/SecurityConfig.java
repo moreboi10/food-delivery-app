dcc9630e3ea9e59d9f088cd33f74be0bb3bd25a9
morebo1 <sanketmore.39410@gamil.com>
2026-09-06 11:37:05 +0530

    fixing cors issuse

--- delivery-app-api/src/main/java/in/sanketmore/delivery/app/api/config/SecurityConfig.java
+++ delivery-app-api/src/main/java/in/sanketmore/delivery/app/api/config/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig {
 
     private UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174","https://foodies-one-bay.vercel.app/","https://foodies-adminpanel-xi.vercel.app/"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174","https://foodies-one-bay.vercel.app","https://foodies-adminpanel-xi.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
         config.setAllowCredentials(true);
